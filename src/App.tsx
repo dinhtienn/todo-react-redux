a879434540aa81bd0e36a37abfc673d3d7603149
Tien Nguyen <dinhtiennguyen.1202@gmail.com>
2024-03-16 13:18:12 +0700

    Firebase deployment versionn

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,25 @@ import TodoList from "./components/TodoList";
 import Filters from "./components/Filters";
 import "./App.css";
 
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyCWeplQe5pVySan-Up39bbvk3t7UZDSsN0",
+  authDomain: "kane-todo-redux.firebaseapp.com",
+  projectId: "kane-todo-redux",
+  storageBucket: "kane-todo-redux.appspot.com",
+  messagingSenderId: "1044972861790",
+  appId: "1:1044972861790:web:4e57df7c793bac63f877cf",
+};
+
+// Initialize Firebase
+// const app = initializeApp(firebaseConfig);
+initializeApp(firebaseConfig);
+
 const { Title } = Typography;
 
 function App() {
